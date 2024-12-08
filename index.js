ae198d708b430414244f53c6caa4c4db1e77bdc8
Kuzey88 <127623406+Kuzey88@users.noreply.github.com>
2024-12-09 01:13:29 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const TelegramBot = require("node-telegram-bot-api");
+const fs = require("fs");
+
+// Token'ınızı ortam değişkenlerinden alın
+const token = process.env.TELEGRAM_BOT_TOKEN;
+
+// Botu polling yöntemi ile başlatın
+const bot = new TelegramBot(token, { polling: true });
+
+// .txt dosyasındaki argo kelimeleri okuyup bir diziye dönüştürün
+const argoKelimeler = fs
+  .readFileSync("argo_kelimeler.txt", "utf-8")
+  .split("\n")
+  .map((kelime) => kelime.trim().toLowerCase())
+  .filter((kelime) => kelime.length > 0);
+
+// Mesajları dinleyin ve argo kelime kontrolü yapın
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const mesaj = msg.text;
+
+  // Mesajın argo kelime içerip içermediğini kontrol et
+  if (mesaj) {
+    const mesajKucukHarf = mesaj.toLowerCase(); // Büyük/küçük harf duyarlılığını kaldırmak için
+
+    for (let kelime of argoKelimeler) {
+      if (mesajKucukHarf.includes(kelime)) {
+        // Eğer argo kelime bulunduysa kullanıcıya uyarı gönder
+        bot.sendMessage(
+          chatId,
+          "Lütfen mesajlarınızda argo kelimeler kullanmayın.",
+        );
+        break; // İlk argo kelime bulduğunda döngüden çık
+      }
+    }
+  }
+});
